09b8dc42bad9b3cf5fb79cc12a9f2d668991ca13
Josh <joshuajohnwallace@gmail.com>
2020-03-09 13:14:59 +1100

    making a bigger array and filling edges with true will work better

--- src/app/GamePanel.java
+++ src/app/GamePanel.java
@@ -5,29 +5,39 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.*;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.border.BevelBorder;
 
 public class GamePanel extends JPanel{
 	
-	private static final long serialVersionUID = -761825656979462027L;
-	private static boolean canvas[][] = new boolean[12][24];
-	private static int yPosition = 0;
-	private static int xPosition = 0;
-	private static TetrominoBag bag = new TetrominoBag();
-	private static Tetromino currentPiece;
+	private final long serialVersionUID = -761825656979462027L;
+	private boolean canvas[][] = new boolean[16][26];
+	private int yPosition = 2;
+	private int xPosition = 3;
+	private TetrominoBag bag = new TetrominoBag();
+	private Tetromino currentPiece;
+	private final int MAX_X = 12;
+	private final int MAX_Y = 23;
+	private final int MIN_X = 1;
+	private final int MIN_Y = 1;
+	
 	private  Timer speed = new Timer(500 , new RepaintListener());
 
 
 	public GamePanel() {
-		setPreferredSize(new Dimension(330, 720));
+		setPreferredSize(new Dimension(300, 720));
 		setBackground(Color.black);
+		setBorder(BorderFactory.createLineBorder(Color.blue));
 		addKeyListener(new ControlListener());
+		
 		setFocusable(true);
 		newGame();
 	}
 	public void newGame() {
-		for(int i =0; i < 12; i++)
+		for(int i =0; i < MAX_X; i++)
 			Arrays.fill(canvas[i], false);
 		bag.randomise();
 		currentPiece = bag.getCurrent();
@@ -45,23 +55,19 @@ public class GamePanel extends JPanel{
 	}
 	public void nextPiece() {	
 		currentPiece = bag.getNext();
-		yPosition = 0;
-		xPosition = 0;
+		yPosition = MIN_Y;
+		xPosition = MIN_X;
 		for(int x = 0 ; x < currentPiece.length(); x++){
 			if(canvas[xPosition][yPosition]) {
 				speed.stop();
 				newGame();
 			}			
 		}
-		for(int x = 0 ; x < currentPiece.length(); x++){
-			for(int y = 0; y < currentPiece.length(); y++) {
-				canvas[xPosition + x][yPosition + y] = currentPiece.value(x, y);
-			}
-		}
+		insertPiece();
 		repaint();		
 	}
 	public void moveFall() {
-		if(yPosition + currentPiece.length() > 23) {
+		if(yPosition + currentPiece.length() -1 == MAX_Y ) {
 			nextPiece();
 		}
 		for(int x = 0 ; x < currentPiece.length(); x++){
@@ -85,27 +91,30 @@ public class GamePanel extends JPanel{
 			}
 		}
 		yPosition++;
-
-		for(int y = 0; y < 23; y++) {
-			if(y < yPosition) {
-				canvas[xPosition][y] = false;
-			}
-		}
 		insertPiece();
 	}
-	public static void moveRight() {
-		if(xPosition == 9) {
+	public void moveRight() {
+		if(xPosition + currentPiece.length() > MAX_X ){
 			return;
 		}
-		for(int y = 0 ; y < currentPiece.length(); y++){			
-				if(canvas[xPosition + currentPiece.length()][yPosition + y] && currentPiece.value(currentPiece.length() -1, y)) {
-					return;					
-				}				
+		//needs to deal with out of bounds, catch somehow
+		for(int x = 0 ; x < (MAX_X - xPosition) && x < currentPiece.length() ; x++){
+			for(int y = 0; y < currentPiece.length(); y++) {
+				/*if(x==0)
+					if(currentPiece.value(currentPiece.length() - (x+ 1), y) && canvas[xPosition + currentPiece.length() - x][yPosition + y] )
+						return;
+				if(currentPiece.value(currentPiece.length() - (x+ 1), y) && canvas[xPosition + currentPiece.length() - x][yPosition + y] && !currentPiece.value(currentPiece.length() - x, y)) {
+					return;
+				}*/
+				if( canvas[xPosition + currentPiece.length() - x][yPosition + y] && canvas[xPosition + currentPiece.length() - x + 1][yPosition + y])
+					return;
 			}
+		}
 
 		for(int x = 0 ; x < currentPiece.length(); x++){
 			for(int y = 0; y < currentPiece.length(); y++) {
-				canvas[xPosition + x][yPosition + y] = false;				
+				if(xPosition + x <= MAX_X && currentPiece.value(x, y))
+					canvas[xPosition + x][yPosition + y] = false;				
 			}
 		}
 		xPosition++;
@@ -117,25 +126,33 @@ public class GamePanel extends JPanel{
 		}
 		
 	}
+	public boolean checkRow() {
+		for(int i =0; i <= MAX_X; i++) {
+			for(int a = 0; a <= MAX_Y; a++) {
+				if()
+			}
+		
+	}
 	public void moveLeft() {
-		if(xPosition == 0 ) {
+		if(xPosition <= MIN_X ) {
 			return;
 		}
-		for(int x = 0 ; x < currentPiece.length(); x++){
+		for(int x = 0 ; x < xPosition && x <  currentPiece.length() ; x++){
 			for(int y = 0; y < currentPiece.length(); y++) {
-				if(currentPiece.value(x, y) && canvas[xPosition + x - 1][yPosition + y]) {
+				if(x == 0 && currentPiece.value(x, y) && canvas[xPosition + x - 1][yPosition + y]) {
 					return;	
 				}
 				else if(x > 0)
 						if(!currentPiece.value(x, y) && currentPiece.value(x - 1, y) && canvas[xPosition - x][yPosition + y]){
-							nextPiece();
+							return;
 				}			
 			}
 		}
 
 		for(int x = 0 ; x < currentPiece.length(); x++){
 			for(int y = 0; y < currentPiece.length(); y++) {
-				canvas[xPosition + x][yPosition + y] = false;				
+				if(xPosition + x <= MAX_X)
+					canvas[xPosition + x][yPosition + y] = false;				
 			}
 		}
 		xPosition--;
@@ -145,21 +162,22 @@ public class GamePanel extends JPanel{
 					canvas[xPosition + x][yPosition + y] = currentPiece.value(x, y);
 			}
 		}
+
 		
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
-		for(int i =0; i < 12; i++) {
-			for(int a = 0; a< 24; a++) {
-				if(canvas[i][a]) {
-					int x = i*30;
-					int y = a*30;
+		for(int x =0; x <= MAX_X; x++) {
+			for(int y = 0; y <= MAX_Y; y++) {
+				if(canvas[x][y]) {
+					int xDraw = (x*30 - 30);
+					int yDraw = (y*30);
 					g.setColor(Color.green);
-					g.fillRect(x, y, 30, 30);
+					g.fillRect(xDraw, yDraw, 30, 30);
 					g.setColor(Color.YELLOW);
-					g.drawRect(x, y, 30, 30);
+					g.drawRect(xDraw, yDraw, 30, 30);
 				}
 			}
 		}
@@ -184,6 +202,7 @@ public class GamePanel extends JPanel{
 			if(event.getKeyCode() == KeyEvent.VK_RIGHT) {
 				moveRight();
 				repaint();
+
 			}
 			//left
 			if(event.getKeyCode() == KeyEvent.VK_LEFT) {
